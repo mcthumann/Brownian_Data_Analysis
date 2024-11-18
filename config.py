Scaling fixes

config.py
@@ -2,21 +2,26 @@ import math
 import scipy.constants as const
 
 SIM = True
+SAMPLE = 100
+
 SAVE = False
 BASE_PATH = r"C:\Users\Cole Thumann\Desktop\Lab_Repos\MarkovianEmbedding\position_velocity_data.csv"
-# BASE_PATH = r"C:\Users\Cole Thumann\Desktop\LabData\20241107\high power_last"
+# BASE_PATH = r"\\tsclient\TESTFOLDER\20241114\1_5"
 
 NUM_FILES = 1
 TRACKS_PER_FILE = 1
 
 # PSD FIT PARAMS
-MIN_BOUND=1e4
+MIN_BOUND=1e3
 MAX_BOUND=1e7
 NUM_LOG_BINS=500
+
 # initial Guesses
 K_GUESS = 1
 A_GUESS = 3e-6
 V_GUESS = 1
+
+M_GUESS = 5e-14
 
 ACF = True
 
@@ -39,7 +44,6 @@ class Config:
             stop=None,
             start=None
     ):
-        print("A is " +str(a))
         self.a = a
         self.eta = eta
         self.rho_silica = rho_silica

fitting.py
@@ -43,7 +43,7 @@ def VACF_fitting_func(t, m, K, a):
     # Find the roots
     roots = np.roots(coefficients)
 
-    vacf_complex = 1e-7 *sum(
+    vacf_complex = (const.k * 293 / m) * sum(
         (z ** 3 * scipy.special.erfcx(z * np.sqrt(t))) /
         (np.prod([z - z_j for z_j in roots if z != z_j])) for z in roots
     )
@@ -164,12 +164,12 @@ def fit_data(dataset, avg=True):
     plt.yscale("log")
     plt.show()
 
-    optimal_mass = VACF_fitting(times, vacf[1:], optimal_parameters.x[0], optimal_parameters.x[1])
-    vacf_fit = VACF_fitting_func(times, optimal_mass.x[0], 10, 3e-6)
+    optimal_mass = VACF_fitting(times, vacf[1:], 1e-1, 3e-6)
+    vacf_fit = VACF_fitting_func(times, optimal_mass.x[0], 1e-1, 3e-6)
     print("MASS FOUND = ", optimal_mass.x[0])
 
     plt.plot(times, vacf[1:])
-    plt.plot(times, vacf_fit/(const.k * 293 / 3.82e-14), label="fit")
+    plt.plot(times, vacf_fit, label="fit")
     plt.xscale("log")
     plt.legend()
     plt.show()

main.py
@@ -27,22 +27,24 @@ def run():
         if SAVE:
             save_results(results, filename)
 
-        # Plot the data
+        # # Plot the data
         plot_results(results, folder)
-
-        K = 10  # Example trap strength
-        a = 3e-6  # Example particle radius in meters
-        m = 3.8170350741115986e-14
-
-        times = np.logspace(-10, -5, 6000)
-        # Calculate PSD
-        vacf_values = VACF_fitting_func(times, m, K, a)
-        # Plot
-        plt.plot(times, vacf_values/(const.k * 293 / 3.82e-14), label='vacf')
-        plt.xscale('log')
-        plt.legend()
-        plt.title('VACF function test')
-        plt.show()
+        # for m in np.logspace(-15, -12, 20):
+        #     K = 1e-1  # Example trap strength
+        #     a = 3e-6  # Example particle radius in meters
+        #     # m = 3.8170350741115986e-14
+        #
+        #     times = np.logspace(-10, -5, 6000)
+        #     # Calculate PSD
+        #     vacf_values = VACF_fitting_func(times, m, K, a)
+        #
+        #     # Plot
+        #     plt.plot(times, vacf_values, label='mass'+str(m))
+        # # plt.legend()
+        # plt.xscale('log')
+        #
+        # plt.title('VACF 2 function test')
+        # plt.show()
 
         # Fit the results
         fit_data(results)

series_processing.py
@@ -1,7 +1,17 @@
 import numpy as np
 import scipy
+from tqdm import tqdm
 from scipy.fft import fft, ifft, fftfreq
-from config import ACF, BIN, SINC, HAMMING, BIN_NUM, RECT_WINDOW, SAVE, SIM
+from config import ACF, BIN, SINC, HAMMING, BIN_NUM, RECT_WINDOW, SAMPLE, SIM
+
+
+def compute_VACF_time_domain(v_series):
+    n = len(v_series)
+    vacf = np.zeros(n)
+    for lag in tqdm(range(n), desc="VACF Compute"):
+        vacf[lag] = np.dot(v_series[:n - lag], v_series[lag:]) / (
+                n - lag)  # Normalize by number of overlapping terms
+    return vacf
 
 def autocorrelation(signal):
     # Using FFT for efficient computation of autocorrelation
@@ -19,7 +29,8 @@ def bin_data(series, bin_size):
 def downsample_log_space(xdata, ydata, num_bins):
     # Logarithmically space the bins
     log_bins = np.logspace(np.log10(xdata[0]), np.log10(xdata[-1]), num_bins)
-    # Digitize the data (assign each data point to a bin)
+    # Digitize the data
+    # (assign each data point to a bin)
     bin_indices = np.digitize(xdata, log_bins)
     # Calculate the average x and y for each bin
     xdata_binned = [xdata[bin_indices == i].mean() for i in range(1, len(log_bins))]
@@ -108,9 +119,9 @@ def process_series(series, conf):
         series = apply_hamming_window(series)
 
     if SIM:
-        frequency, psd = scipy.signal.periodogram(series, 1 / (conf.timestep), scaling="density")
+        frequency, psd = scipy.signal.periodogram(series, 1 / (conf.timestep * SAMPLE), scaling="density")
         frequency /= conf.t_c
-        psd *= (conf.x_c ** 2) * conf.t_c
+        psd *= conf.t_c
     else:
         frequency, psd = scipy.signal.periodogram(series, 1 / (conf.sampling_rate*BIN_NUM), scaling="density")
     v_freq, v_psd_local = scipy.signal.periodogram(v_series, 1 / (conf.timestep * BIN_NUM), scaling="density")
@@ -118,7 +129,8 @@ def process_series(series, conf):
 
     if ACF:
         acf = autocorrelation(series)
-        v_acf = autocorrelation(v_series)
+        v_acf = compute_VACF_time_domain(v_series)
+
     else:
         acf = 0
         v_acf = 0